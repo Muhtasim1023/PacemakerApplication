23a80fcc6e997b02fa32ff13d6476af522a9ee7a
Muhtasim1023 <41923336+Muhtasim1023@users.noreply.github.com>
2021-10-23 18:17:36 -0400

    Update PacemakerGUILogin.py

--- PacemakerGUILogin.py
+++ PacemakerGUILogin.py
@@ -5,8 +5,6 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 
 import json
 
-sm = ScreenManager()
-
 # Define our different screens
 class CreateNewAccountWindow(Screen):
     pass
@@ -23,6 +21,7 @@ class WindowManager(ScreenManager):
 
 # Welcome page Screen
 class MainApp(MDApp):
+    
     def build(self):
         self.theme_cls.theme_style = "Dark"
         self.theme_cls.primary_palette = "DeepPurple"
@@ -33,19 +32,25 @@ class MainApp(MDApp):
         username = self.root.ids.login.user.text 
         password = self.root.ids.login.password.text
 
-        # Search Username and PassWord in UserInfo.json
-        # 
-        print("Username: ", username)
-        print("Password: ", password)
+        with open('UserInfo.json', 'r') as r:
+            data = json.loads(r.read())
+            for i in data["UserInfo"]:
+                if i["Username"] == username and i["Password"] == password:
+                    self.root.ids.login.user.text = ""
+                    self.root.ids.login.password.text = ""
+                    return "MainWindow"
+
+        return "LoginWindow"
+        
 
     def logNewUser(self):
+
         name = self.root.ids.newUser._name.text 
         username = self.root.ids.newUser._user.text 
         password = self.root.ids.newUser._pass.text
 
-        #data = {"Name: ": name, "Username: ": username, "Password: ": password }
-
         database = []
+        newUser = {"Username": username, "Name": name, "Password": password}
 
         with open('UserInfo.json', 'r') as r:
             data = json.loads(r.read())
@@ -53,20 +58,23 @@ class MainApp(MDApp):
                 database.append(i)
             r.close()
 
-        newUser = {"Username": username, "Name": name, "Password": password}
-
         with open('UserInfo.json', 'w') as w:
             if len(database) < 10:
                 if not newUser in database:
                     database.append(newUser)
+                    self.root.ids.newUser._name.text = "" 
+                    self.root.ids.newUser._user.text = ""
+                    self.root.ids.newUser._pass.text = ""
                     print("New user added.")
+                    json.dump({"UserInfo": database}, w, indent = 2)
+                    return "LoginWindow"
                 else:
                     print("User already exists.")     
             else:
                 #POP UP
                 print("Too Many Users FOP!")   
             json.dump({"UserInfo": database}, w, indent = 2)
-
+            return "CreateNewAccountWindow"
 
 
 if __name__ == '__main__':
