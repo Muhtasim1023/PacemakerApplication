3d6b747e886f431ac1cdd0cac5da595a465be00c
Muhtasim1023 <41923336+Muhtasim1023@users.noreply.github.com>
2021-10-23 02:33:57 -0400

    Update PacemakerGUILogin.py

--- PacemakerGUILogin.py
+++ PacemakerGUILogin.py
@@ -14,6 +14,9 @@ class CreateNewAccountWindow(Screen):
 class LoginWindow(Screen):
     pass
 
+class MainWindow(Screen):
+    pass
+
 class WindowManager(ScreenManager):
     pass
 
@@ -30,18 +33,40 @@ class MainApp(MDApp):
         username = self.root.ids.login.user.text 
         password = self.root.ids.login.password.text
 
-        # Search Username and PassWord in savedUser.txt
+        # Search Username and PassWord in UserInfo.json
+        # 
         print("Username: ", username)
         print("Password: ", password)
 
     def logNewUser(self):
         name = self.root.ids.newUser._name.text 
         username = self.root.ids.newUser._user.text 
-        password = self.root.ids.newUser._pass.text 
-        data = {"Name: ": name, "Username: ": username, "Password: ": password }
+        password = self.root.ids.newUser._pass.text
 
-        with open('savedUser.txt', 'w') as f:
-            json.dump(data, f)
+        #data = {"Name: ": name, "Username: ": username, "Password: ": password }
+
+        database = []
+
+        with open('UserInfo.json', 'r') as r:
+            data = json.loads(r.read())
+            for i in data["UserInfo"]:
+                database.append(i)
+            r.close()
+
+        newUser = {"Username": username, "Name": name, "Password": password}
+
+        with open('UserInfo.json', 'w') as w:
+            if len(database) < 10:
+                if not newUser in database:
+                    database.append(newUser)
+                    print("New user added.")
+                else:
+                    print("User already exists.")     
+            else:
+                #POP UP
+                print("Too Many Users FOP!")   
+            json.dump({"UserInfo": database}, w, indent = 2)
+
 
 
 if __name__ == '__main__':
